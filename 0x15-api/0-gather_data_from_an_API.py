ceadfd70f96b99110df02888bbe8c9c104580012
chineduCoded <chinedujohn172gmail.com>
2023-04-25 13:53:36 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,30 @@
 Script that, using this REST API, for a given employee ID, returns
 information about his/her TODO list progress.
 """
-import requests
 import sys
+import requests
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com/"
-    employee = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [
-            task.get("title")
-            for task in todos
-            if task.get("completed") is True
-    ]
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee.get("name"), len(completed), len(todos)))
+    if len(sys.argv) < 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        exit(1)
 
-    [print("\t {}".format(title)) for title in completed]
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get("{}/{}/todos".format(base_url, employee_id))
+
+    if response.status_code != 200:
+        print("Error: Invalid employee ID")
+        exit(1)
+
+    data = response.json()
+    employee_name = requests.get("{}/{}".format(
+        base_url, employee_id)).json()["name"]
+    total_tasks = len(data)
+    completed_tasks = sum([1 for task in data if task["completed"]])
+    task_titles = [task["title"] for task in data if task["completed"]]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, completed_tasks, total_tasks))
+    for title in task_titles:
+        print("\t {}".format(title))
