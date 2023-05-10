a3445265baf6f03232b076836f6326ab7866497f
chineduCoded <chinedujohn172gmail.com>
2023-05-10 04:15:14 -0700

    Refactored the codes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-Count words in all hot posts of a given Reddit subreddit.
+Count words in all hot posts of a given Reddit subreddit
 """
 import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """
-    Prints counts of given words found in hot posts of a given subreddit.
+    """Prints counts of given words found in hot posts of a given subreddit.
     Args:
         subreddit (str): The subreddit to search.
         word_list (list): The list of words to search for in post titles.
-        instances (dict): Dictionary containing counts of word occurrences.
+        instances (obj): Key/value pairs of words/counts.
         after (str): The parameter for the next page of the API results.
         count (int): The parameter of results matched thus far.
     """
@@ -31,6 +30,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         if response.status_code == 404:
             raise Exception
     except Exception:
+        print("")
         return
 
     results = results.get("data")
@@ -39,14 +39,18 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     for c in results.get("children"):
         title = c.get("data").get("title").lower().split()
         for word in word_list:
-            if word.lower() in title and \
-               not any((title_word in ['.', ',', '!', '?'] or title_word.endswith(('.', ',', '!', '?')))
-                       for title_word in title if title_word != word.lower()):
-                instances[word.lower()] = instances.get(word.lower(), 0) + title.count(word.lower())
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
     if after is None:
-        instances = dict(sorted(instances.items(), key=lambda x: (-x[1], x[0])))
-        for word, count in instances.items():
-            print("{}: {}".format(word, count))
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
