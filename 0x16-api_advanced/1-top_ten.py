2b05dfb24eac33d1ec62dffbb10dbf5431f231fd
chineduCoded <chinedujohn172gmail.com>
2023-05-10 03:58:29 -0700

    Changed user agent

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -20,7 +20,7 @@ def top_ten(subreddit):
         If the subreddit is invalid, the function will print None.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
